7c40104d58eec460a35f6a00849758b7d51613a0
as.grebennikov <as.grebennikov@tensor.ru>
2017-12-13 17:20:16 +0700

    starter commit

---
+++ app/src/main/java/com/projects/asgrebennikov/repetitor/SentenceActivity.java
@@ -0,0 +1,13 @@
+package com.projects.asgrebennikov.repetitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SentenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sentence);
+    }
+}
