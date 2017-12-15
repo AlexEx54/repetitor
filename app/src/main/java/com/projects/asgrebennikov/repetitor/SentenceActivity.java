a09d29866dda36a207c7898cc473f70ffcd14d85
as.grebennikov <as.grebennikov@tensor.ru>
2017-12-15 20:50:36 +0700

    added api keys

--- app/src/main/java/com/projects/asgrebennikov/repetitor/SentenceActivity.java
+++ app/src/main/java/com/projects/asgrebennikov/repetitor/SentenceActivity.java
@@ -2,12 +2,19 @@ package com.projects.asgrebennikov.repetitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SentenceActivity extends AppCompatActivity {
+
+    private static String yandexKey = "https://translate.yandex.net/api/v1.5/tr.json/translate";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sentence);
+
+        URL url = new URL("https://translate.yandex.net/api/v1.5/tr.json/translate?" +
+                          "key=" + yandexKey);
     }
 }
