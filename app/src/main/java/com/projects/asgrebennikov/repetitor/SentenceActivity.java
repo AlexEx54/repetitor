4606fa7adc8f4884e0334685f000f0c41b5c3900
as.grebennikov <as.grebennikov@tensor.ru>
2017-12-19 16:26:34 +0700

    new yandex api key

--- app/src/main/java/com/projects/asgrebennikov/repetitor/SentenceActivity.java
+++ app/src/main/java/com/projects/asgrebennikov/repetitor/SentenceActivity.java
@@ -2,19 +2,50 @@ package com.projects.asgrebennikov.repetitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
+
+
+class MyRunnable implements Runnable {
+
+    private static String yandexKey = "dict.1.1.20171219T092115Z.b4d251fe6793335a.ce9863aa4d1660707da362b3a1e2122fa7db659c";
+
+    public void run() {
+        try {
+            URL url = new URL("https://dictionary.yandex.net/api/v1/dicservice.json/lookup?" +
+                    "key=" + yandexKey + "&" +
+                    "lang=en-ru" + "&" +
+                    "text=корова");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            in.read();
+
+            Scanner s = new Scanner(in).useDelimiter("\\A");
+            String result = s.hasNext() ? s.next() : "";
+            result = "";
+
+        } catch ( Exception e) {
+            String what = e.getMessage();
+            what = "";
+        }
+    }
+
+}
 
 public class SentenceActivity extends AppCompatActivity {
-
-    private static String yandexKey = "https://translate.yandex.net/api/v1.5/tr.json/translate";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sentence);
 
-        URL url = new URL("https://translate.yandex.net/api/v1.5/tr.json/translate?" +
-                          "key=" + yandexKey);
+        MyRunnable myRunnable = new MyRunnable();
+        Thread t = new Thread(myRunnable);
+        t.start();
     }
 }
