bd9501e28639e86b8f0d3c9cbb8480e4eeff7f73
as.grebennikov <as.grebennikov@tensor.ru>
2018-03-29 20:17:16 +0700

    created db_info table

--- app/src/main/java/backend/DatabaseImpl.java
+++ app/src/main/java/backend/DatabaseImpl.java
@@ -1,12 +1,54 @@
 package backend;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by as.grebennikov on 28.03.18.
  */
 
 public class DatabaseImpl implements Database {
 
-    public void Open(String version) {}
+    public void Open(String version) {
+        db_ = SQLiteDatabase.openOrCreateDatabase("repetitor", null);
+        String dbVersion = GetDatabaseVersion();
+        if (dbVersion == null || dbVersion != version)
+        {
+            CreateDatabase(version);
+        }
+    }
 
     public void SaveWord(WordContext wordContext) {}
+
+
+    private String GetDatabaseVersion()
+    {
+        assert(db_ != null);
+        assert(db_.isOpen());
+
+        try {
+            Cursor sql_result = db_.rawQuery("SELECT version FROM db_info", null);
+            sql_result.moveToFirst();
+            String version = sql_result.getString(sql_result.getColumnIndexOrThrow("version"));
+            return  version;
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
+
+
+    private void CreateDatabase(String version) {
+        assert(db_ != null);
+        assert(db_.isOpen());
+
+        db_.execSQL("CREATE TABLE db_info (version TEXT NOT NULL)");
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("version", version);
+        db_.insertOrThrow("db_info",null, contentValues);
+    }
+
+    private SQLiteDatabase db_ = null;
 }
